94ba57eba6173305b8f6051ae1f7a8d6f5b5a00c
Susan Emerick <Suzmrck@Susans-MacBook-Pro.local>
2016-04-06 21:45:24 -0400

    Learned about intergers and strings

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
 int main() {
-    std::cout << "Hello Remi \n";
+
+    string walter;
+
+    cout << "Hello Remi \n";
+    walter = "Remi";
+    cout << "Hello " + walter;
+    cout << "Hello walter";
+
+
+    int a = 7;
+    int b = 5;
+    int c = a * b;
+    cout << a << " \\ \"multiplied\" by " << b << " equals " << c;
+
+
 }
