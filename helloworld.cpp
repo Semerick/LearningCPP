598eb4dc312606b90eb9795635d6e1e0114c0562
Susan Emerick <susanmrck@gmail.com>
2016-04-06 22:26:46 -0400

    Learning how to bool and if/then statements

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,19 +4,38 @@
 using namespace std;
 
 int main() {
-
+    
+    /* Learning how to define strings
     string walter;
-
+    
     cout << "Hello Remi \n";
     walter = "Remi";
     cout << "Hello " + walter;
     cout << "Hello walter";
 
-
+    Learning how to define integers
+    
     int a = 7;
     int b = 5;
     int c = a * b;
     cout << a << " \\ \"multiplied\" by " << b << " equals " << c;
+    */
 
+    bool remiIsCute = true;
 
+    if (remiIsCute) { 
+        cout << "duh we knew that \n";
+    } else { 
+        cout << "no, he actually is \n";
+    }
+
+    int numTimesHit = -5;
+    if (numTimesHit > 0) {
+        cout << "Bad Walter! \n";
+    } else if (numTimesHit == 0) { 
+        cout << "Good boy, Walter! \n";
+    } else { 
+        cout << "I don't know how that's possible, Walter \n";
+    }
+        
 }
